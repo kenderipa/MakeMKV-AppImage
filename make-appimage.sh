4b42155d8e95fe37e7621ef1db13109a9196ebaa
kenderipa <52325831+kenderipa@users.noreply.github.com>
2025-11-24 02:39:43 +0100

    Update icon and desktop entry paths in script

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,11 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/128x128/apps/makemkv.png
+export DESKTOP=/usr/share/applications/makemkv.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/share/makemkv
 
 # Additional changes can be done in between here
 
